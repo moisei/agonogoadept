30fb8158a376df7f236dc8c9f27963c619b6d1a6
Moisei <moisei@nowhere.com>
2023-05-15 01:25:57 +0300

    add HOME button

--- app/src/main/java/com/moisei/gng/MainActivity.kt
+++ app/src/main/java/com/moisei/gng/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.moisei.gng
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,6 +10,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.MutableState
@@ -16,6 +19,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.moisei.gng.ui.theme.AGoNoGoAdeptTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     private val handler = Handler(Looper.getMainLooper())
@@ -27,19 +35,31 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AGoNoGoAdeptTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Box(
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Surface(color = MaterialTheme.colorScheme.background) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(color = goState.value)
+                                .clickable {
+                                    changeToNoGoState()
+                                }
+                        )
+                    }
+                    Button(
+//                        onClick = { finish() }, // or startActivity(Intent(this, HomeScreenActivity::class.java))
+                        onClick = { startActivity(Intent(this@MainActivity, HomeScreenActivity::class.java)) },
                         modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = goState.value)
-                            .clickable {
-                                changeToNoGoState()
-                            }
-                    )
+                            .align(Alignment.BottomCenter)
+                            .fillMaxWidth()
+                            .height((LocalConfiguration.current.screenHeightDp * 0.1).dp)
+                    ) {
+                        Text(text = "HOME")
+                    }
                 }
             }
         }
+
     }
 
     private fun changeToNoGoState() {
